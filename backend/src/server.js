ce07e503d262e32e9b070ea688e1b7958aeb288d
Thanusha Annamaneni <annamenenithanusha@gmail.com>
2026-06-15 00:44:49 +0530

    Database connected successfully

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db/db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Splitwise Clone API Running"
-  });
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      success: true,
+      time: result.rows[0]
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message
+    });
+  }
 });
 
 const PORT = 3001;
@@ -17,4 +27,3 @@ const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
